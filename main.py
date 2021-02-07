21af6f4a0f440171caa5e98c6069130b5d57f135
NazarovVal <78682462+NazarovVal@users.noreply.github.com>
2021-02-07 14:39:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def prog(chis):
+    for i  in range(1000,10000):
+        vrem = i
+        strok = []
+        des = 1
+        good = []
+        for j in range(1,5):
+            strok.append(vrem % (10 * des) // des)
+            des *= 10
+        for j in range(3):
+            good.append(int(strok[j]) + int(strok[j+1]))
+        good.pop(good.index(min(good)))
+        otv = int(str(min(good))+str(max(good)))
+        if otv == chis:
+            return i
+print(prog(int(input())))
